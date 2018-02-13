59f5d3fcf6ceb196e8e80c9732997ed8313b9cf5
Jeric Frank Sasil <jericfrank@gmail.com>
2018-02-13 14:55:37 +0800

    chore(Auth0): configure

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Auth0 library
+        Auth0\Login\LoginServiceProvider::class,
     ],
 
     /*
@@ -209,6 +211,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        // Auth0 library
+        'Auth0' => Auth0\Login\Facade\Auth0::class,
     ],
 
 ];
